b78bff89b8fb4b92035937e0a2f801acbd4740c4
Shrikant <sawantshrikant07@gmail.com>
2023-05-10 13:03:58 +0530

    [Shrikant] add:CompareTwoLines

---
+++ LineComparisonComputation281BATCH/Line.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Line : IComparable<Line>
+{
+    private double x1, y1, x2, y2;
+
+    public Line(double x1, double y1, double x2, double y2)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double CalculateLength()
+    {
+        return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    }
+
+    public int CompareTo(Line otherLine)
+    {
+        double length1 = CalculateLength();
+        double length2 = otherLine.CalculateLength();
+
+        return length1.CompareTo(length2);
+    }
+}
--- LineComparisonComputation281BATCH/Program.cs
+++ LineComparisonComputation281BATCH/Program.cs
@@ -1,36 +1,37 @@
-﻿using System;
-
-public class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Line Comparison Computation Program!");
 
-        double x1 = 2;
-        double y1 = 3;
-        double x2 = 4;
-        double y2 = 5;
+        Line line1 = new Line(2, 3, 4, 5);
+        Line line2 = new Line(1, 2, 5, 7);
 
-        double length1 = CalculateLineLength(x1, y1, x2, y2);
-
-        double a1 = 1;
-        double b1 = 2;
-        double a2 = 5;
-        double b2 = 7;
-
-        double length2 = CalculateLineLength(a1, b1, a2, b2);
+        double length1 = line1.CalculateLength();
+        double length2 = line2.CalculateLength();
 
         Console.WriteLine("The Length of Line 1 is: " + length1);
         Console.WriteLine("The Length of Line 2 is: " + length2);
 
-        // Check equality using the equals method
-        bool areEqual = length1.Equals(length2);
-        Console.WriteLine("Are the lines equal? " + areEqual);
-    }
 
-    private static double CalculateLineLength(double x1, double y1, double x2, double y2)
-    {
-        double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-        return length;
+        int comparisonResult = line1.CompareTo(line2);
+
+        if (comparisonResult < 0)
+        {
+            Console.WriteLine("Line 1 is shorter than Line 2");
+        }
+        else if (comparisonResult > 0)
+        {
+            Console.WriteLine("Line 1 is longer than Line 2");
+        }
+        else
+        {
+            Console.WriteLine("Line 1 is equal in length to Line 2");
+        }
     }
 }
+
+
+
+
+
