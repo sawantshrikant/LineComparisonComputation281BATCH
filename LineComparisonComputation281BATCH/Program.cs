ee6751432bc9262a074f4962be10561e5cd1dcee
Shrikant <sawantshrikant07@gmail.com>
2023-05-10 12:09:06 +0530

    [Shrikant] add:EqualityOfTwoLines

--- LineComparisonComputation281BATCH/Program.cs
+++ LineComparisonComputation281BATCH/Program.cs
@@ -5,19 +5,32 @@ public class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Line Comparison Computation Program!");
+
         double x1 = 2;
         double y1 = 3;
         double x2 = 4;
         double y2 = 5;
 
-        double length = CalculateLineLength(x1, y1, x2, y2);
+        double length1 = CalculateLineLength(x1, y1, x2, y2);
 
-        Console.WriteLine("The Length of Line is:" + length);
+        double a1 = 1;
+        double b1 = 2;
+        double a2 = 5;
+        double b2 = 7;
+
+        double length2 = CalculateLineLength(a1, b1, a2, b2);
+
+        Console.WriteLine("The Length of Line 1 is: " + length1);
+        Console.WriteLine("The Length of Line 2 is: " + length2);
+
+        // Check equality using the equals method
+        bool areEqual = length1.Equals(length2);
+        Console.WriteLine("Are the lines equal? " + areEqual);
     }
 
     private static double CalculateLineLength(double x1, double y1, double x2, double y2)
     {
-        double length = (Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         return length;
     }
 }
