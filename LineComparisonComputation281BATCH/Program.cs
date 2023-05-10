8de0e78f22d8205666809ecfd5ff71ff8e7a53ef
Shrikant <sawantshrikant07@gmail.com>
2023-05-10 11:06:15 +0530

    [Shrikant] add:CalculateLengthOfLine

--- LineComparisonComputation281BATCH/Program.cs
+++ LineComparisonComputation281BATCH/Program.cs
@@ -1,7 +1,23 @@
-﻿public class Program
+﻿using System;
+
+public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Line Comparison Computation Program!");
+        double x1 = 2;
+        double y1 = 3;
+        double x2 = 4;
+        double y2 = 5;
+
+        double length = CalculateLineLength(x1, y1, x2, y2);
+
+        Console.WriteLine("The Length of Line is:" + length);
+    }
+
+    private static double CalculateLineLength(double x1, double y1, double x2, double y2)
+    {
+        double length = (Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        return length;
     }
 }
