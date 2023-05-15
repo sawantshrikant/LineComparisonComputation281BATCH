4272aa5cf9acfa9c487dbc6e9b70a83620fd063a
sawantshrikant <124336267+sawantshrikant@users.noreply.github.com>
2023-05-15 23:17:38 +0530

    Update Program.cs

--- LineComparisonComputation281BATCH/Program.cs
+++ LineComparisonComputation281BATCH/Program.cs
@@ -1,23 +1,29 @@
-﻿using System;
+using System;
 
 public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Line Comparison Computation Program!");
-        double x1 = 2;
-        double y1 = 3;
-        double x2 = 4;
-        double y2 = 5;
+
+        // Prompt the user to enter coordinates
+        Console.WriteLine("Enter the coordinates of the first point (x1, y1):");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        int y1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the coordinates of the second point (x2, y2):");
+        int x2 = Convert.ToInt32(Console.ReadLine());
+        int y2 = Convert.ToInt32(Console.ReadLine());
 
         double length = CalculateLineLength(x1, y1, x2, y2);
 
-        Console.WriteLine("The Length of Line is:" + length);
+        Console.WriteLine("The Length of Line is: " + length);
     }
 
-    private static double CalculateLineLength(double x1, double y1, double x2, double y2)
+    private static double CalculateLineLength(int x1, int y1, int x2, int y2)
     {
-        double length = (Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         return length;
     }
 }
+
